5e4535cee77d79bb9c76a1f51bc2a5d84edbdf41
SUSHMITHA1402 <gummadis999@gmail.com>
2023-01-20 20:13:24 +0530

    Third Commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,9 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    static int countChee =0;
-    static int countTop =0;
-    static int countTake =0;
+    int countChee =0;
+    int countTop =0;
+    int countTake =0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -22,44 +22,60 @@ public class Pizza {
 
     public int getPrice(){
 
-        return this.price;
+        return price;
     }
 
     public void addExtraCheese(){
         // your code goes here
         countChee++;
+        if(countChee==1){
+            price+=80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
         countTop++;
+        if(isVeg){
+            price+=70;
+        }
+        else{
+            price+=120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
         countTake++;
+        if(countTake==1){
+            price+=20;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        String out ="Base Price Of The Pizza: "+price+"\n";
+        String out="";
+        if(isVeg){
+            out ="Base Price Of The Pizza: 300"+"\n";
+        }
+        else{
+            out ="Base Price Of The Pizza: 400"+"\n";
+        }
+
         if(countChee>0){
             out+="Extra Cheese Added: 80"+"\n";
-            price+=80;
+
         }
         if(countTop>0){
             if(isVeg){
                 out+="Extra Toppings Added: 70"+"\n";
-                price+=70;
             }
             else{
                 out+="Extra Toppings Added: 120"+"\n";
-                price+=120;
             }
         }
         if(countTake>0){
             out+="Paperbag Added: 20"+"\n";
-            price+=20;
         }
         bill = out+"Total Price: "+price+"\n";
         return this.bill;
