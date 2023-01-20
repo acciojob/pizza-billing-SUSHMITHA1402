cd72b0c1d8d10629e6f087842ed708b59f6f47ba
SUSHMITHA1402 <gummadis999@gmail.com>
2023-01-20 20:35:06 +0530

    fourth

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -13,10 +13,10 @@ public class Pizza {
         this.isVeg = isVeg;
         // your code goes here
         if(isVeg){
-            price = 300;
+            this.price = 300;
         }
         else{
-            price = 400;
+            this.price = 400;
         }
     }
 
